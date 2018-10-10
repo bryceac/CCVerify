eeb7c82f7f80662cce1cbc36929ac68a93053d2a
Bryce Campbell <bryce@bryces-imac.local>
2018-10-09 18:43:56 -0700

    add forgotten UI element and connect UI elements to code.

--- colorContrast/ViewController.swift
+++ colorContrast/ViewController.swift
@@ -9,7 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorAreaController: UISegmentedControl!
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var redValue: UITextField!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var greenValue: UITextField!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var blueValue: UITextField!
+    @IBOutlet weak var hexCode: UITextField!
+    @IBOutlet weak var textView: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func reset(_ sender: UIButton) {
+    }
+    
 }
 
