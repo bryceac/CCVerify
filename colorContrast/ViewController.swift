32f5823c5a54e1d838b6557155240925deaaec34
Bryce Campbell <bryce@bryces-imac.local>
2018-10-09 18:57:19 -0700

    add in function to do work.

--- colorContrast/ViewController.swift
+++ colorContrast/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var colorAreaController: UISegmentedControl!
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var textView: UILabel!
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var redValue: UITextField!
     @IBOutlet weak var greenSlider: UISlider!
@@ -18,7 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueSlider: UISlider!
     @IBOutlet weak var blueValue: UITextField!
     @IBOutlet weak var hexCode: UITextField!
-    @IBOutlet weak var textView: UILabel!
+    
     
     
     override func viewDidLoad() {
@@ -30,7 +31,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // updateBackgrounColor is used to change the background color of the preview
+    func updateBackgroundColor(withRed: CGFloat, green: CGFloat, blue: CGFloat) {
+        colorView.layer.backgroundColor = UIColor(red: withRed/255, green: green/255, blue: blue/255, alpha: 1).cgColor
+    }
+    
+    func updateTextColor(withRed: CGFloat, green: CGFloat, blue: CGFloat) {
+        textView.textColor = UIColor(red: withRed/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    // reset function is a function that will set things back to the default values
     @IBAction func reset(_ sender: UIButton) {
     }
     
